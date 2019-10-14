6cc496f534d301a8ea7f231caf652b7c41309ac4
Derek Cortez <derekcortez16@gmail.com>
2019-10-14 03:14:12 -0500

    codigo comentado, pero no funca  :c

--- p1.asm
+++ p1.asm
@@ -16,39 +16,42 @@ msj db 13,10,'Suma: ','$'
 ciclo:				;Etiqueta donde estará nuestro ciclo
 	mov ax, @data
 	mov ds, ax	
-	cmp [contador],10
-	
-	je salir;
+	cmp [contador],10 ; Compara contador=10
+	je salir; Si contador = 10, se va a la etiqueta salir
 
 	;Potencia
-	mov al, [contador] 
-	mov bl, [contador]
-	mul bl
-	mov [tmp], al
+	mov al, [contador] ; Pasa valor de contador a al
+	mov bl, [contador] ; Pasa valor de contador a bl
+	mul bl	; al = al*bl
+	mov [tmp], al ; Pasa valor de al a una variable temporal
 
 
 	;Segunda parte
-	mov al, [contador]
-	mov bl, 3
-	mul bl
-	mov bl, al
-	mov al, 2
-	div bl
-	mov [tmp1], al
+	mov al, [contador] ; Pasa valor de contador a al
+	mov bl, 3	; bl = 3
+	mul bl	; al = al * bl(3)
+	mov bl, al; bl = al
+	mov al, 2;	al = 2
+	div bl	;al= al/bl
+	mov [tmp1], al; Pasa valor de al a otra variable temporal 
 
 	;Suma +10
-	mov [tmp2], 10
+	mov [tmp2], 10	; A una tercera variable temporal, se le asigna el valor 10
 
-	add dl, [tmp]
-	add dl, [tmp1]	
-	add dl, [tmp2]
+	add dl, [tmp]	; Se agrega el valor de tmp(Potencia) a dl
+	add dl, [tmp1]	; Se agrega el valor de tmp1(Parte 2) a dl
+	add dl, [tmp2]	; Se le agrega el valor de tmp2(Suma +10) a dl
 
-	add dl,30h
+	; POR LO TANTO dl = tmp + tmp1 + tmp2
+
+	add dl,30h ; Se agregan 30 en valor hexadecimal para tratar de sacar el valor verdadero en hexa
+	
+	;Impresion
 	mov ah,02h
 	int 21h
 
-	inc [contador]
-jmp ciclo
+	inc [contador]; Incrementa el contador en 1 (contador++)
+jmp ciclo ; Salta al ciclo de nuevo
 
 
 
